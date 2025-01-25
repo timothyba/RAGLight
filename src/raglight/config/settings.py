1c87e8a7cedb6f316a6fa05c62e86fbc200b40fe
Bessouat40 <roman.bessouat@orange.fr>
2025-01-25 11:59:47 +0100

    format file

--- src/raglight/config/settings.py
+++ src/raglight/config/settings.py
@@ -17,7 +17,9 @@ class Settings:
     HUGGINGFACE = "HuggingFace"
     DEFAULT_LLM = "llama3"
     DEFAULT_EMBEDDINGS_MODEL = "all-MiniLM-L6-v2"
-    DEFAULT_SYSTEM_PROMPT=contextual_prompt = """
+    DEFAULT_SYSTEM_PROMPT = (
+        contextual_prompt
+    ) = """
         # I am a Context-Aware Assistant:
         - My primary role is to utilize the provided context (e.g., documents, code, or descriptions) to answer user questions accurately and effectively.
         - I adapt my responses based on the given context, aiming to provide relevant, clear, and actionable information.
