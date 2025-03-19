c7c79d365633c2a25db40a664d4635c036a55d2a
Bessouat40 <roman.bessouat@orange.fr>
2025-03-19 19:13:22 +0100

    fix: debug build() agentic rag

--- examples/simple_agentic_rag_example.py
+++ examples/simple_agentic_rag_example.py
@@ -1,12 +1,21 @@
 from raglight.config.settings import Settings
-from raglight.rag.agentic_rag import AgenticRAG
+from raglight.rag.simple_agentic_rag_api import AgenticRAGPipeline
 from raglight.config.agentic_rag_config import AgenticRAGConfig
 from raglight.config.vector_store_config import VectorStoreConfig
 from raglight.config.settings import Settings
+from raglight.models.data_source_model import FolderSource
+from raglight.models.data_source_model import GitHubSource
 from dotenv import load_dotenv
+
+
 
 load_dotenv()
 Settings.setup_logging()
+
+knowledge_base=[
+    FolderSource(path="<path to your folder with pdf>/knowledge_base"),
+    GitHubSource(url="https://github.com/Bessouat40/RAGLight")
+    ]
 
 persist_directory = './defaultDb'
 model_embeddings = Settings.DEFAULT_EMBEDDINGS_MODEL
@@ -30,9 +39,12 @@ config = AgenticRAGConfig(
             # api_base = ... # If you have a custom client URL
             # num_ctx = ... # Max context length
             # verbosity_level = ... # Default = 2
+            # knowledge_base = knowledge_base
         )
 
-agenticRag = AgenticRAG(config, vector_store_config)
+agenticRag = AgenticRAGPipeline(config, vector_store_config)
+
+agenticRag.build()
 
 response = agenticRag.generate("Please implement for me AgenticRAGPipeline inspired by RAGPipeline and AgenticRAG and RAG")
 
