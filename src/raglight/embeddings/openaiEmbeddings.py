a8a166f168f534ab7825146044bb07b0be401f49
Bessouat40 <roman.bessouat@orange.fr>
2025-03-21 10:35:19 +0100

    new version

--- src/raglight/embeddings/openaiEmbeddings.py
+++ src/raglight/embeddings/openaiEmbeddings.py
@@ -10,31 +10,31 @@ from langchain_openai import OpenAIEmbeddings
 class OpenAIEmbeddingsModel(EmbeddingsModel):
     """
     Concrete implementation of the EmbeddingsModel for OpenAI models.
-    
+
     This class provides a specific implementation of the abstract `EmbeddingsModel` for
     loading and using OpenAI embeddings.
-    
+
     Attributes:
         model_name (str): The name of the OpenAI model to be loaded.
     """
-    
+
     def __init__(self, model_name: str) -> None:
         """
         Initializes an OpenAIEmbeddingsModel instance.
-        
+
         Args:
             model_name (str): The name of the OpenAI model to load.
         """
         super().__init__(model_name)
-    
+
     @override
     def load(self) -> OpenAIEmbeddings:
         """
         Loads the OpenAI embeddings model.
-        
+
         This method overrides the abstract `load` method from the `EmbeddingsModel` class
         and initializes the OpenAI embeddings model with the specified `model_name`.
-        
+
         Returns:
             OpenAIEmbeddings: The loaded OpenAI embeddings model.
         """
@@ -42,5 +42,5 @@ class OpenAIEmbeddingsModel(EmbeddingsModel):
         return OpenAIEmbeddings(
             model=self.model_name,
             openai_api_base=Settings.DEFAULT_OPENAI_CLIENT,
-            openai_api_key=Settings.OPENAI_API_KEY
+            openai_api_key=Settings.OPENAI_API_KEY,
         )
